6fe5b3ffc305abd333c66e9f6edee33f59adce36
Kevin Sakia Putra <kevinsakia.p@gmail.com>
2025-05-06 23:34:00 +0700

    cuki

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,12 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
+      '@typescript-eslint/ban-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      // Disable type checking for params incompatibility
+      '@typescript-eslint/ban-types': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
